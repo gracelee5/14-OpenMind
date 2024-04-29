7fcc9249fad0963ee6d33658308ecd4b97d8d92e
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2024-04-29 14:55:55 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended', // eslint-plugin-jsx-a11y 추가
+    'plugin:prettier/recommended', // eslint-plugin-prettier 추가
+    'plugin:react-hooks/recommended', // eslint-plugin-react-hooks 추가
+  ],
+
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
