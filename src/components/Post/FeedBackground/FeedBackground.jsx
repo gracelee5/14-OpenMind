4bdb097562d8f057034c93fa4cb8a473d81a798d
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2024-04-30 16:14:09 +0900

    feat: 포스트 페이지 마크업

--- src/components/Post/FeedBackground/FeedBackground.jsx
+++ src/components/Post/FeedBackground/FeedBackground.jsx
@@ -6,27 +6,38 @@ import link from '../../../images/link-img.svg';
 import kakao from '../../../images/kakao-img.svg';
 import facebook from '../../../images/facebook-img.svg';
 import profile from '../../../images/profile-img.svg';
+import empty from '../../../images/empty.svg';
+import message from '../../../images/Messages.svg';
 
 function FeedBackground() {
   return (
     <>
       <Header>
-        <BackgroundImg src={BgImg} />
         <Logo src={logo} />
       </Header>
-      <Section>
-        <ProfileImg src={profile} />
-        <UserName>아초는고양이</UserName>
-        <LinkSection>
-          <ShareButton src={link} />
-          <ShareButton src={kakao} />
-          <ShareButton src={facebook} />
-        </LinkSection>
-      </Section>
+      <BodyContainer>
+        <Section>
+          <ProfileImg src={profile} />
+          <UserName>아초는고양이</UserName>
+          <LinkSection>
+            <ShareButton src={link} />
+            <ShareButton src={kakao} />
+            <ShareButton src={facebook} />
+          </LinkSection>
+        </Section>
+        <PostContainer>
+          <QuestionStatus>
+            <img src={message} alt='message img' />
+            아직 질문이 없습니다.
+          </QuestionStatus>
+        </PostContainer>
+        <QuestionFormButton>질문 작성하기</QuestionFormButton>
+      </BodyContainer>
     </>
   );
 }
 export default FeedBackground;
+
 const Header = styled.div`
   width: 100%;
   background-color: #f8f8f8;
@@ -35,19 +46,20 @@ const Header = styled.div`
   background-image: url(${BgImg});
   background-repeat: no-repeat;
   background-position: center;
-`;
-const BackgroundImg = styled.img`
   position: relative;
-  width: 1200px;
   height: 234px;
-  margin: 0 auto;
-  opacity: 0;
+`;
+const BodyContainer = styled.div`
+  background-color: #f9f9f9;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 const Section = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: -100px;
 `;
 const Logo = styled.img`
   position: absolute;
@@ -59,6 +71,8 @@ const Logo = styled.img`
 const ProfileImg = styled.img`
   width: 136px;
   height: 136px;
+  margin-top: -100px;
+  z-index: 1;
 `;
 const UserName = styled.p`
   align-items: center;
@@ -67,12 +81,61 @@ const UserName = styled.p`
   font-weight: 400;
   font-size: 32px;
   color: #000000;
+  height: 40px;
+  margin-top: 20px;
 `;
 const LinkSection = styled.div`
   display: flex;
+  margin-top: -20px;
   gap: 8px;
 `;
 const ShareButton = styled.img`
   width: 40px;
   height: 40px;
+`;
+const PostContainer = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px;
+  gap: 16px;
+  width: 716px;
+  min-height: 330px;
+  background: #f5f1ee;
+  border: 1px solid #c7bbb5;
+  border-radius: 16px;
+  margin-top: 50px;
+  background-image: url(${empty});
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+const QuestionStatus = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-family: 'Actor';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  height: 25px;
+  color: #542f1a;
+`;
+const QuestionFormButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  margin: 70px 24px 24px auto;
+  background: #542f1a;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 200px;
+  font-family: 'Actor';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 25px;
+  color: #ffffff;
+  height: 54px;
+  width: 208px;
 `;
