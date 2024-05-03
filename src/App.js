d3f1471bf2f224b0e619b0b0ae6f58b80835fd3c
YUKIJOO <koreala1@naver.com>
2024-05-03 14:22:11 +0900

    "feature/list" - 누구에게 질문할까요? (body태그 질문 추가)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ListHeader from './components/ListHeader';
-import ListPage from './components/ListPage';
+import DropDownButton from './components/DropdownButton';
 
 function App() {
   return (
     <BrowserRouter>
-      <ListHeader />
-
       <div>
         <Routes>
-          <Route index element={<ListPage />} />
+          <Route index element={<ListHeader />} />
         </Routes>
       </div>
+
+      <body>
+        <div>누구에게 질문할까요?</div>
+
+        <div>
+          <DropDownButton />
+        </div>
+      </body>
     </BrowserRouter>
   );
 }
