c8a1e6616e2c827a10e3da9ce21071114099afdd
YUKIJOO <koreala1@naver.com>
2024-05-03 19:08:18 +0900

    "feature/list - 헤더 답변하러 가기 버튼 스타일 수정!"

--- src/components/AnswerButton.jsx
+++ src/components/AnswerButton.jsx
@@ -5,21 +5,21 @@ const AButton = styled.button`
 display: inline-flex;
     align-items: center;
     outline: none;
-    border: none;
+    border: solid;
     border-radius: 4px;
-    color: white;
+    color: #542F1A;
     font-weight: bold;
     cursor: pointer;
     padding-left: 1rem;
     padding-right: 1rem;
     height: 2.25rem;
     font-size: 1rem;
-    background: #228be6;
+    background: #E4D5C9;
     &:hover{
-        background: #339af0;
+        background: #F5F1EE;
     }
     &:active{
-        background: #1c7ed6;
+        background: #F5F1EE;
         & + & {
             margin-left: 1rem;
         }
