0bdba6c64458f5ea1c33f800fd11a9c94862a6da
YUKIJOO <koreala1@naver.com>
2024-05-10 21:43:14 +0900

    "feature/list" - usercard 컴포넌트 스타일 수정 - 미완성

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,32 +1,64 @@
+import { Link } from 'react-router-dom';
 import QuestionMark from '../images/QuestionMark.svg';
 import styled from 'styled-components';
+
+const UserCardGlobal = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 940px;
+  height: 474px;
+  top: 261px;
+  left: 130px;
+`;
 
 const UserCardBox = styled.div`
   width: 220px;
   height: 187px;
-  border: 1px solid #000;
-  box-sizing: border-box;
+  border: 3px solid #000;
+  display: inline-block;
+  border-radius: 8px;
+`;
+
+const UserCardImage = styled.div`
+  position: relative;
+  top: 10px;
+  bottom: 170px;
+  width: 60px;
+  height: 60px;
+`;
+
+const CountNumber = styled.span`
+  position: relative;
+  left: 100px;
+`;
+
+const CatQuestion = styled.div`
+  bottom: 200px;
 `;
 
 function UserInformation({ usercarddata }) {
   return (
-    <UserCardBox>
-      <img src={usercarddata.imageSource} alt={usercarddata.name} />
-      <div>
+    <UserCardGlobal>
+      <UserCardBox>
+        <UserCardImage>
+          <img src={usercarddata.imageSource} alt={usercarddata.name} />
+        </UserCardImage>
         <h1>{usercarddata.name}</h1>
-        <p>
+        <CatQuestion>
           <img src={QuestionMark} alt='받은 질문' />
-          받은 질문
-        </p>
-        <p>count</p>
-      </div>
-    </UserCardBox>
+          <Link to='/post'> 받은 질문 </Link>
+        </CatQuestion>
+        <CountNumber> 9개 </CountNumber>
+      </UserCardBox>
+    </UserCardGlobal>
   );
 }
 
 function UserCard({ users }) {
   return (
-    <ul>
+    <>
+      {' '}
       {users &&
         users.map(function (usercarddata) {
           return (
@@ -36,7 +68,7 @@ function UserCard({ users }) {
           );
         })}
       ;
-    </ul>
+    </>
   );
 }
 
