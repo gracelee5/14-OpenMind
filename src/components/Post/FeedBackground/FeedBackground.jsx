9ee748e49b69afe60e78e2f201ce93d3ecdebba4
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2024-05-10 21:03:11 +0900

    style: 반응형 디자인 1

--- src/components/Post/FeedBackground/FeedBackground.jsx
+++ src/components/Post/FeedBackground/FeedBackground.jsx
@@ -112,6 +112,13 @@ const Header = styled.div`
   background-position: center;
   position: relative;
   height: 234px;
+  @media only screen and (min-width: 768px) and (max-width: 1200px) {
+    width: 100%;
+  }
+
+  @media only screen and (max-width: 767px) {
+    height: 177px;
+  }
 `;
 const BodyContainer = styled.div`
   background-color: #f9f9f9;
@@ -125,6 +132,9 @@ const Section = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  @media only screen and (max-width: 767px) {
+    height: 177px;
+  }
 `;
 const Logo = styled.img`
   position: absolute;
@@ -149,6 +159,9 @@ const UserName = styled.p`
   color: #000000;
   height: 40px;
   margin-top: 20px;
+  @media only screen and (max-width: 767px) {
+    width: 327px;
+  }
 `;
 const LinkSection = styled.div`
   display: flex;
@@ -171,6 +184,12 @@ const PostContainer = styled.div`
   border: 1px solid #c7bbb5;
   border-radius: 16px;
   margin-top: 50px;
+  @media only screen and (min-width: 768px) and (max-width: 1200px) {
+    width: 704px;
+  }
+  @media only screen and (max-width: 767px) {
+    width: 327px;
+  }
 `;
 const Empty = styled.img`
   width: 150px;
