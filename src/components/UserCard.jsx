b7d3467e78681422d20e6b39b33d4c128b49cd53
YUKIJOO <koreala1@naver.com>
2024-05-10 21:11:49 +0900

    "feature/list" - 데이터 불러와서 화면에 렌더링하는 컴포넌트(usercard) 생성(임시-mock데이터)

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,131 +1,43 @@
-import styled from 'styled-components';
-import Catpicture from '../images/Catpictured.svg';
 import QuestionMark from '../images/QuestionMark.svg';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-const AnswerBox = styled.button`
+const UserCardBox = styled.div`
   width: 220px;
   height: 187px;
-  border-radius: 16px;
-  background-color: white;
-  position: relative;
+  border: 1px solid #000;
+  box-sizing: border-box;
 `;
 
-const CatBox = styled.div`
-  position: absolute;
-  align-items: flex-end;
-  font-family: 'Pretendard';
-  font-size: 20px;
-  width: 180;
-  line-height: 25px;
-`;
-
-const ImgBox = styled.div`
-  position: absolute;
-  top: 10px;
-  bottom: 170px;
-  width: 60px;
-  height: 60px;
-`;
-
-const CatQuestion = styled.div`
-  position: absolute;
-  bottom: 10px;
-  top: 150px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const CountNumber = styled.span`
-  position: relative;
-  left: 100px;
-`;
-
-const GlobalSection = styled.div`
-  position: relative;
-  left: 200px;
-  right: 200px;
-  width: 940px;
-  height: 474px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: cetner;
-`;
-
-function AnswerFeed() {
+function UserInformation({ usercarddata }) {
   return (
-    <GlobalSection>
-      <AnswerBox>
-        <ImgBox>
-          <img src={Catpicture} alt='고양이 사진' />
-        </ImgBox>
-
-        <CatBox>아초는 고양이</CatBox>
-
-        <CatQuestion>
-          <img src={QuestionMark} alt='questionmark' />
-          <Link to='/Post'> 받은 질문 </Link>
-          <CountNumber> 9개 </CountNumber>
-        </CatQuestion>
-      </AnswerBox>
-
-      <AnswerBox>
-        <ImgBox>
-          <img src={Catpicture} alt='고양이 사진' />
-        </ImgBox>
-
-        <CatBox>아초는 고양이</CatBox>
-
-        <CatQuestion>
-          <img src={QuestionMark} alt='questionmark' />
-          <Link to='/Post'> 받은 질문 </Link>
-          <CountNumber> 9개 </CountNumber>
-        </CatQuestion>
-      </AnswerBox>
-
-      <AnswerBox>
-        <ImgBox>
-          <img src={Catpicture} alt='고양이 사진' />
-        </ImgBox>
-
-        <CatBox>아초는 고양이</CatBox>
-
-        <CatQuestion>
-          <img src={QuestionMark} alt='questionmark' />
-          <Link to='/Post'> 받은 질문 </Link>
-          <CountNumber> 9개 </CountNumber>
-        </CatQuestion>
-      </AnswerBox>
-      <AnswerBox>
-        <ImgBox>
-          <img src={Catpicture} alt='고양이 사진' />
-        </ImgBox>
-
-        <CatBox>아초는 고양이</CatBox>
-
-        <CatQuestion>
-          <img src={QuestionMark} alt='questionmark' />
-          <Link to='/Post'> 받은 질문 </Link>
-          <CountNumber> 9개 </CountNumber>
-        </CatQuestion>
-      </AnswerBox>
-      <AnswerBox>
-        <ImgBox>
-          <img src={Catpicture} alt='고양이 사진' />
-        </ImgBox>
-
-        <CatBox>아초는 고양이</CatBox>
-
-        <CatQuestion>
-          <img src={QuestionMark} alt='questionmark' />
-          <Link to='/Post'> 받은 질문 </Link>
-          <CountNumber> 9개 </CountNumber>
-        </CatQuestion>
-      </AnswerBox>
-    </GlobalSection>
+    <UserCardBox>
+      <img src={usercarddata.imageSource} alt={usercarddata.name} />
+      <div>
+        <h1>{usercarddata.name}</h1>
+        <p>
+          <img src={QuestionMark} alt='받은 질문' />
+          받은 질문
+        </p>
+        <p>count</p>
+      </div>
+    </UserCardBox>
   );
 }
 
-export default AnswerFeed;
+function UserCard({ users }) {
+  return (
+    <ul>
+      {users &&
+        users.map(function (usercarddata) {
+          return (
+            <div key={usercarddata}>
+              <UserInformation usercarddata={usercarddata} />
+            </div>
+          );
+        })}
+      ;
+    </ul>
+  );
+}
+
+export default UserCard;
