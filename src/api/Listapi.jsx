8a56bf2ad8b35321649f1d44f10b68ad57e316fd
YUKIJOO <koreala1@naver.com>
2024-05-14 10:21:23 +0900

    "feature/list"- pagination 구현 및 스타일링!

--- src/api/Listapi.jsx
+++ src/api/Listapi.jsx
@@ -1,6 +1,6 @@
-export async function ListApi() {
+export async function ListApi(newOffset = 0) {
   const response = await fetch(
-    'https://openmind-api.vercel.app/6-14/subjects/'
+    `https://openmind-api.vercel.app/6-14/subjects/?limit=8&offset=${newOffset}`
   );
   const body = await response.json();
   return body;
--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -20,41 +20,69 @@ const DropDownButtonBox = styled.div`
   padding: 30px;
 `;
 
-const PaginationDesign = styled.div`
+const MyPaginate = styled(ReactPaginate).attrs({
+  activeClassName: 'active',
+})`
+  margin: 50px 16px;
   display: flex;
   justify-content: center;
+  list-style-type: none;
+  padding: 0 5rem;
+
+  li a {
+    border-radius: 7px;
+    padding: 0.1rem 1rem;
+    cursor: pointer;
+  }
+  li.previous a,
+  li.next a {
+    color: #62b6b7;
+  }
+  li.active a {
+    color: #91cccd;
+    font-weight: 700;
+    min-width: 32px;
+  }
+  li.disabled a {
+    color: 15px;
+  }
+  li.disable,
+  li.disabled a {
+    cursor: default;
+  }
+  @media (max-width: 600px) {
+    font-size: 15px;
+  }
 `;
 
 function ListPage() {
   const [items, setItems] = useState([]);
+  const [maxCount, setMaxCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 8;
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = items.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(maxCount / itemsPerPage);
 
   useEffect(() => {
     // 컴포넌트가 마운트될 때 ListApi 호출
     const fetchData = async () => {
-      const { results } = await ListApi();
+      const { results, count } = await ListApi(itemOffset);
       setItems(results);
+      setMaxCount(count);
     };
 
     fetchData(); // fetchData 함수 호출
-  }, []); // 빈 배열을 전달하여 한 번만 실행되도록 설정
+  }, [itemOffset]); // 빈 배열을 전달하여 한 번만 실행되도록 설정
 
   useEffect(() => {
     // items 값이 변경될 때 UserCard 호출
     UserCard(items);
   }, [items]);
 
-  const [itemOffset, setItemOffset] = useState(0);
-  const itemsPerPage = 8;
-  const endOffset = itemOffset + itemsPerPage;
-  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-  const currentItems = items.slice(itemOffset, endOffset);
-  const pageCount = Math.ceil(items.length / itemsPerPage);
-
   const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % items.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
+    const newOffset = event.selected * itemsPerPage;
     setItemOffset(newOffset);
   };
 
@@ -72,17 +100,23 @@ function ListPage() {
         <UserCard results={items} />
       </main>
 
-      <PaginationDesign>
-        <ReactPaginate
-          firstPage={currentItems}
-          nextLabel='next >'
-          onPageChange={handlePageClick}
-          pageRangeDisplayed={5}
-          pageCount={pageCount}
-          previousLabel='< previous'
-          renderOnZeroPageCount={null}
-        />
-      </PaginationDesign>
+      <MyPaginate
+        firstPage={currentItems}
+        nextLabel='next >'
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel='< previous'
+        renderOnZeroPageCount={null}
+        containerClassName='pagination justify-content-center'
+        pageClassName='page-item'
+        pageLinkClassName='page-link'
+        previousClassName='page-item'
+        previousLinkClassName='page-link'
+        nextClassName='page-item'
+        nextLinkClassName='page-link'
+        activeClassName='active'
+      />
     </>
   );
 }
